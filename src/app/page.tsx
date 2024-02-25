8fa82f3b23e01c296626bb6799179e8767268002
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2024-02-26 01:08:51 +0200

    update copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,54 +3,61 @@ import NewsletterForm from "./newsletter-form";
 export default function HomePage() {
   return (
     <div className="flex flex-col gap-10">
-      <p>A new era in Islamic Law Research.</p>
+      <div className="flex flex-col gap-10 text-[hsl(4,53%,90%)] [&_a]:text-white [&_a]:underline [&_strong]:text-white">
+        <h1>A new era in Islamic Law research.</h1>
 
-      <p>
-        The world of Islamic law is vast and rich in knowledge but almost
-        inaccessible to researchers.
-      </p>
+        <p>
+          For centuries, Islamic Law has been a cornerstone of legal, social,
+          and ethical guidance across diverse communities around the globe.
+          However, the vast repositories of knowledge within the tradition have
+          remained inaccessible due to the <strong>physical limitations</strong>{" "}
+          of texts and the <strong>scattered nature</strong> of resources.
+        </p>
 
-      <p>
-        Limited access to physical copies requires researchers to travel to
-        far-flung libraries or private collections scattered across different
-        continents, from Cairo to Istanbul, from Rabat to Jakarta.
-      </p>
+        <p>
+          Researchers have to travel to far-flung libraries or private
+          collections scattered across different continents. And when they do
+          get their hands on these texts, they are confronted with an even
+          greater challenge: the absence of{" "}
+          <strong>advanced research tools</strong>.
+        </p>
 
-      <p>
-        The absence of a comprehensive catalogue results in wasted hours spent
-        locating pertinent texts, often navigating through complex and obscure
-        references.
-      </p>
+        <p>
+          Researchers have to manually go through volumes of texts to find
+          specific references or legal rulings. The process is{" "}
+          <strong>time-consuming</strong> and{" "}
+          <strong>frustratingly slow</strong>.
+        </p>
 
-      <p>The process is time-consuming and frustratingly slow.</p>
+        <p>We’re here to change this.</p>
 
-      <div>
-        Enter Seemore, a new project set to change the landscape in three ways:
+        <p>
+          We’re transforming Islamic Law research by <strong>digitizing</strong>{" "}
+          thousands of classical texts, <strong>organizing</strong> them for
+          easy navigation, and <strong>developing</strong> powerful search tools
+          and algorithms. By doing so, we hope to unlock new insights and make
+          the field accessible to researchers worldwide.
+        </p>
+
+        <p>
+          We’re a group of <strong>technologists</strong> and{" "}
+          <strong>lawyers</strong> bringing expertise from Harvard, UPenn,
+          Twitter, and Quran.com to solve this problem.
+        </p>
+
+        <p>
+          Join us along this journey by subscribing to updates below, following
+          our work on{" "}
+          <a href="https://github.com/seemorg" target="_blank">
+            GitHub
+          </a>
+          , and if you’re up for sharing some feedback:{" "}
+          <a href="mailto:early-access@digitalseem.org" target="_blank">
+            request early access
+          </a>{" "}
+          to our first product.
+        </p>
       </div>
-
-      <ul className="list">
-        <li>
-          Digitize thousands of legal texts and make them easily accessible
-        </li>
-        <li>Create a universal catalogue for Islamic law research</li>
-        <li>Build tools for researchers to efficiently navigate the texts</li>
-      </ul>
-
-      <p>
-        We’re a group of technologists and lawyers bringing expertise from
-        Harvard, UPenn, Twitter, and Quran.com. Join us for this journey and
-        stay tuned for updates.
-      </p>
-
-      {/* <div className="mt-2">
-          <Signature className="w-36" />
-          <p className="mt-3">{config.ceo.name}</p>
-          <p>CEO, {config.org}</p>
-
-          <Button asChild variant="link" className="text-white">
-            <a href={`mailto:${config.ceo.email}`}>{config.ceo.email}</a>
-          </Button>
-        </div> */}
 
       <div className="mt-20">
         <NewsletterForm />
