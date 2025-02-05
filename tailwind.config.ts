641014f93c895ac9d6fd4a24107e6f3d0c9414c2
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2025-02-06 00:06:37 +0300

    fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,75 +6,51 @@ const config = {
   content: ["./src/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
-  	container: {
-  		center: true,
-  		padding: '2rem',
-  		screens: {
-  			'2xl': '1400px'
-  		}
-  	},
-  	extend: {
-  		fontFamily: {
-  			sans: [
-  				'var(--font-roboto)',
-                    ...fontFamily.sans
-                ],
-  			scheherazade: [
-  				'var(--font-scheherazade)',
-                    ...fontFamily.serif
-                ]
-  		},
-  		colors: {
-  			primary: {
-  				background: '#C9F905',
-  				foreground: '#273E3D'
-  			},
-  			secondary: '#171D1DB2'
-  		},
-  		keyframes: {
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			},
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
-  		},
-  		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out',
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
-  		},
-  		screens: {
-  			xs: '480px'
-  		}
-  	}
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-roboto)", ...fontFamily.sans],
+        scheherazade: ["var(--font-scheherazade)", ...fontFamily.serif],
+      },
+      colors: {
+        primary: {
+          background: "#C9F905",
+          foreground: "#273E3D",
+        },
+        secondary: "#171D1DB2",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      screens: {
+        xs: "480px",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 } satisfies Config;
